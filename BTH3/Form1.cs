067910cdd2034d9a3e300cd689a169f805a98ea2
hngocc <2354050077ngoc@ou.edu.vn>
2025-03-19 00:33:36 +0700

    Add files via upload

---
+++ BTH3/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PheptinhTrenPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnCong.Text;
+            LopPhanSo ps1, ps2;
+            try
+            {
+                ps1 = new LopPhanSo(int.Parse(txtTu1.Text), int.Parse(txtMau1.Text));
+                ps2 = new LopPhanSo(int.Parse(txtTu2.Text), int.Parse(txtMau2.Text));
+                LopPhanSo kq = ps1.Cong(ps2);
+                txtTu3.Text = kq.Tuso.ToString();
+                txtMau3.Text = kq.Mauso.ToString();
+            }
+            catch (Exception ex)
+            {
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnTru.Text;
+            LopPhanSo ps1, ps2;
+            try
+            {
+                ps1 = new LopPhanSo(int.Parse(txtTu1.Text), int.Parse(txtMau1.Text));
+                ps2 = new LopPhanSo(int.Parse(txtTu2.Text), int.Parse(txtMau2.Text));
+                LopPhanSo kq = ps1.Tru(ps2);
+                txtTu3.Text = kq.Tuso.ToString();
+                txtMau3.Text = kq.Mauso.ToString();
+            }
+            catch (Exception ex)
+            {
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnNhan.Text;
+            LopPhanSo ps1, ps2;
+            try
+            {
+                ps1 = new LopPhanSo(int.Parse(txtTu1.Text), int.Parse(txtMau1.Text));
+                ps2 = new LopPhanSo(int.Parse(txtTu2.Text), int.Parse(txtMau2.Text));
+                LopPhanSo kq = ps1.Nhan(ps2);
+                txtTu3.Text = kq.Tuso.ToString();
+                txtMau3.Text = kq.Mauso.ToString();
+            }
+            catch (Exception ex)
+            {
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            lbDau.Text = btnChia.Text;
+            LopPhanSo ps1, ps2;
+            try
+            {
+                ps1 = new LopPhanSo(int.Parse(txtTu1.Text), int.Parse(txtMau1.Text));
+                ps2 = new LopPhanSo(int.Parse(txtTu2.Text), int.Parse(txtMau2.Text));
+                LopPhanSo kq = ps1.Chia(ps2);
+                txtTu3.Text = kq.Tuso.ToString();
+                txtMau3.Text = kq.Mauso.ToString();
+            }
+            catch (Exception ex)
+            {
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtTu1.Clear();
+            txtMau1.Clear();    
+            txtTu2.Clear();                 
+            txtMau2.Clear();
+            txtTu3.Clear();
+            txtMau3.Clear();
+                
+
+        }
+    }
+}
---
+++ BTH3/LopPhanSo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PheptinhTrenPS
+{
+    internal class LopPhanSo
+    {
+        int tuso, mauso;
+        public LopPhanSo()
+        {
+             tuso = 0;
+             mauso = 1;
+        }  
+        public LopPhanSo(int t, int m)
+        {
+            tuso = t;
+            mauso = m==0 ? 1 : m;
+
+        }
+        public int Tuso
+        {
+           get { return tuso; }
+            set { tuso =value; }
+                
+         }
+
+        public int Mauso
+        {
+            get { return mauso; }
+            set { mauso = value; }
+
+        }
+
+        private void toiGian()
+        {
+            int UC = Class1.UCLN(tuso, mauso);
+            if (UC > 0)
+            {
+                tuso /= UC;
+                mauso /= UC;
+            }
+        }
+        public LopPhanSo Cong(LopPhanSo p)
+        {
+            LopPhanSo kq= new LopPhanSo();
+            kq.tuso = tuso * p.mauso + mauso * p.tuso;
+            kq.mauso= mauso* p.mauso;
+            kq.toiGian();
+            return kq;
+        }
+        public LopPhanSo Tru(LopPhanSo p)
+        {
+            LopPhanSo kq = new LopPhanSo();
+            kq.tuso = tuso * p.mauso - mauso * p.tuso;
+            kq.mauso = mauso * p.mauso;
+            kq.toiGian();
+            return kq;
+        }
+        public LopPhanSo Nhan(LopPhanSo p)
+        {
+            LopPhanSo kq = new LopPhanSo();
+            kq.tuso = tuso  * p.tuso;
+            kq.mauso = mauso * p.mauso;
+            kq.toiGian();
+            return kq;
+        }
+        public LopPhanSo Chia(LopPhanSo p)
+        {
+            LopPhanSo kq = new LopPhanSo();
+            kq.tuso = tuso * p.mauso ;
+            kq.mauso = mauso * p.tuso;
+            kq.toiGian();
+            return kq;
+        }
+
+    }
+}
