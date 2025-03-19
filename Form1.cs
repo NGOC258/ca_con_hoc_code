cef7176c6d58bd72e613aaa94de47d9835263560
hngocc <2354050077ngoc@ou.edu.vn>
2025-03-19 14:35:06 +0700

    chuthich: CHUONG trimg PIC MEW

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quaBong
+{
+    public partial class Form1 : Form
+    {
+        int dx = 10, dy = 13;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (picMew.Left < 0 || picMew.Right > ClientRectangle.Width)
+                dx = -dx;
+            if (picMew.Top < 0 || picMew.Bottom > ClientRectangle.Height)
+                dy = -dy;
+            picMew.Left += dx;
+            picMew.Top += dy;
+
+        }
+    }
+}
